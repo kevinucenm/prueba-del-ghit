129fca47206791abeed83a7e9afcbed142a1708d
kevin <kevin@KEVIN-MORENO>
2023-06-03 16:47:22 -0600

    Agregar archivos de proyecto.

---
+++ prueba del ghit/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba_del_ghit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
